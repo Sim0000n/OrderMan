a7d607093904d3526a817360200d4a2af0500ac5
Sim0000n <mendellovejazz@outlook.com>
2019-12-31 19:44:49 +0800

    first commit

---
+++ orderman_server/src/main/java/pers/simon/orderman/OrdermanApplication.java
@@ -0,0 +1,13 @@
+package pers.simon.orderman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdermanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdermanApplication.class, args);
+    }
+
+}
---
+++ orderman_server/src/test/java/pers/simon/orderman/OrdermanApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.simon.orderman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdermanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
